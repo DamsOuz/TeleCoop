c77f3aebc60c91b004acce014511ccb3fac55b3a
Damien Ouzillou <damien.ouzillou@student-cs.fr>
2025-02-06 12:00:00 +0100

    Feat: detect clic on button + enable button only when name is written

--- app/src/main/java/com/telecoop/telecoop/ui/slideshow/QuizzFragment.java
+++ app/src/main/java/com/telecoop/telecoop/ui/slideshow/QuizzFragment.java
@@ -1,6 +1,9 @@
 package com.telecoop.telecoop.ui.slideshow;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -39,6 +42,38 @@ public class QuizzFragment extends Fragment {
         quizzViewModel.getTextName().observe(getViewLifecycleOwner(), textViewName::setText);
         quizzViewModel.getTextHintName().observe(getViewLifecycleOwner(), textViewHintName::setHint);
         quizzViewModel.getTextButton().observe(getViewLifecycleOwner(), textButton::setText);
+
+        binding.textHintname.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                boolean isEmpty = s.toString().isEmpty();
+                binding.textButtonFirstpageQuizz.setEnabled(!isEmpty);
+            }
+        });
+
+        binding.textButtonFirstpageQuizz.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                //TODO: naviguer vers le fragment de quizz
+                Log.d("Damien", "Clic !");
+            }
+        });
+    }
+
+    @Override
+    public void onStart(){
+        super.onStart();
+        binding.textButtonFirstpageQuizz.setEnabled(false);
     }
 
     @Override
